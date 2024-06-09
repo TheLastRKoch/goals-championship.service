30e09958724f6b1a4d03375f63db171b103332b3
Sergio <theLastRkoch@gmail.com>
2024-06-09 12:18:08 -0600

    Separate the controller logic from the app.py

--- src/app.py
+++ src/app.py
@@ -1,31 +1,20 @@
 from controllers.goals import GoalController, GoalListController
-from controllers.index import IndexController
+from controllers.login import LoginController
 from dotenv import load_dotenv
+from os import environ as env
 from flask import Flask
 import os
-
-from flask import render_template, request, session, redirect, url_for
-from flask.views import MethodView
-from os import environ as env
 
 # Load env variables
 load_dotenv()
 
 
 app = Flask(__name__, template_folder=env["TEMPLATES_PATH"])
-
-@app.route("/login")
-def login():
-    template_path = env["TEMPLATES_PATH"]+"login.jinja2"
-    return render_template("login.jinja2")
-
-@app.route("/login", methods=["POST"])
-def procesing_login():
-    token = request.form["token"]
-    #TODO: Check if token is valid if not show an error
-    return {"msg":"Login successfully"}
-
 app.secret_key = os.getenv("SECRET_KEY")
+
+# Register controllers
+app.add_url_rule('/login', view_func=LoginController.as_view('login'))
+
 
 if __name__ == "__main__":
     app.run(
--- src/controllers/login.py
+++ src/controllers/login.py
@@ -1,3 +1,12 @@
-from flask import render_template, request, session, redirect, url_for
+from flask import render_template, request, redirect
 from flask.views import MethodView
 from os import environ as env
+
+class LoginController(MethodView):
+    def get(self):
+        return render_template("login.jinja2")
+
+    def post(self):
+        token = request.form["token"]
+        #TODO: Check if token is valid if not show an error
+        return {"msg":"Login successfully"}
