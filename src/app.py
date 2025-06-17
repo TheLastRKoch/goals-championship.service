4f303273ecdb555823c705357a090856d1113520
Sergio S <thelastrkoch@gmail.com>
2025-06-17 09:39:40 -0600

    Finished the zenkit service integration backend

--- src/app.py
+++ src/app.py
@@ -15,17 +15,19 @@ app = Flask(__name__, template_folder=env["TEMPLATES_PATH"])
 app.secret_key = os.getenv("SECRET_KEY")
 
 # Register controllers
-app.add_url_rule('/', view_func=IndexController.as_view('index'))
-app.add_url_rule('/account/login', view_func=LoginController.as_view('login'))
-app.add_url_rule('/account/logout', view_func=LogoutController.as_view('logout'))
-app.add_url_rule('/goals', view_func=GoalController.as_view('goals'))
-app.add_url_rule('/goal/list', view_func=GoalListController.as_view('goal_list'))
-app.add_url_rule('/goal/list/download', view_func=DownloadGoalList.as_view('goal_list_download'))
+app.add_url_rule("/", view_func=IndexController.as_view("index"))
+app.add_url_rule("/account/login", view_func=LoginController.as_view("login"))
+app.add_url_rule("/account/logout", view_func=LogoutController.as_view("logout"))
+app.add_url_rule("/goals", view_func=GoalController.as_view("goals"))
+app.add_url_rule("/goal/list", view_func=GoalListController.as_view("goal_list"))
+app.add_url_rule(
+    "/goal/list/download", view_func=DownloadGoalList.as_view("goal_list_download")
+)
 
 
 if __name__ == "__main__":
     app.run(
         host=os.getenv("HOSTNAME"),
         port=int(os.getenv("PORT")),
-        debug=bool(os.getenv("DEBUG"
-                             )))
+        debug=bool(os.getenv("DEBUG")),
+    )
---
+++ src/services/zenkit.py
@@ -0,0 +1,47 @@
+from utils.jmespath import UtilsJMESpath
+from os import environ as env
+import requests
+
+
+class ServiceZenkit:
+    def __init__(self, api_token):
+
+        # Define services
+        self.jmespath = UtilsJMESpath()
+
+        self.headers = {
+            "Zenkit-API-Key": api_token,
+            "Content-Type": "application/json",
+        }
+        self.base_url = env["ZENKIT_BASE_URL"]
+
+        self.items_per_page = int(env["ZENKIT_ITEMS_PER_PAGE"])
+
+    def get_list_of_lists(self):
+        url = self.base_url + "/users/me/workspacesWithLists"
+        response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
+
+        # Format the request
+        return self.jmespath.expression(env["ZENKIT_LIST_QUERY"], response.json())
+
+    def get_entry_list_per_list(self, list_short_id, start_date):
+        skip = 0
+        keep = True
+        url = f"{self.base_url}/lists/{list_short_id}/entries/filter"
+        entry_list = []
+
+        while keep:
+            payload = {"limit": self.items_per_page, "skip": skip}
+            response = requests.post(url, headers=self.headers, json=payload)
+            response.raise_for_status()
+            if response.json() == []:
+                keep = False
+            skip += self.items_per_page
+            entry_list += response.json()
+
+        # Format response
+        return self.jmespath.expression(
+            env["ZENKIT_ENTRIES_QUERY"].replace("@{selected_date}", start_date),
+            entry_list,
+        )
